c55fba937366562c5016d5d204b3354a045dc662
ototadana <ototadana@gmail.com>
2016-08-12 13:27:40 +0900

    Update software to the latest version

--- config/update-plugins.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-rm -fr /var/jenkins_home/war/WEB-INF/plugins/*
-rm -fr /var/jenkins_home/plugins/*.jpi
-
-cp  /usr/share/jenkins/ref/plugins/* /var/jenkins_home/war/WEB-INF/plugins/
